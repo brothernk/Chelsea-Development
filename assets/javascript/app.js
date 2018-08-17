76779c847ac58ad6884d9080becac7d40eb48177
brothernk <brothernk21@hotmail.com>
2018-08-17 15:07:07 -0500

    slideshows and more mobile responsive

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,22 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
 
+    //Click through Slideshow
     var slideIndex = 1;
     showSlides(slideIndex);
 
     function plusSlides(n) {
-    showSlides(slideIndex += n);
-    }
-
-    // Thumbnail image controls
-    function currentSlide(n) {
-    showSlides(slideIndex = n);
+        showSlides(slideIndex += n);
     }
 
     function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("img");
-    if (n > slides.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-    }
-    slides[slideIndex-1].style.display = "block"; 
+        var i;
+        var slides = document.getElementsByClassName("img");
+        if (n > slides.length) {slideIndex = 1} 
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none"; 
+        }
+        slides[slideIndex-1].style.display = "block"; 
     }
 
     $(".fa-angle-left").click(function(){
@@ -31,4 +26,18 @@ $( document ).ready(function() {
     $(".fa-angle-right").click(function(){
         plusSlides(-1);
     });
+
+    // $(window).scroll(function() {scrollFunction()} );
+ 
+    // var aptName = document.getElementById("apt-maple");
+    // var sticky = aptName.offsetTop;
+
+    // function scrollFunction() {
+    //     if (window.pageYOffset >= sticky) {
+    //         aptName.classList.add("sticky")
+    //     } 
+    //     else {
+    //         aptName.classList.remove("sticky");
+    //     }
+    // }
 });
